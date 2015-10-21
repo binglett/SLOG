5bae43e3bc676fd8b621109f2297ebda797b5e51
binglett <blng@sfu.ca>
2015-10-20 17:13:58 -0700

    routed Welcome page to Show Users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,25 @@
 class WelcomeController < ApplicationController
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def create
+		
+	end
+
+	def destroy
+		
+	end
 end
