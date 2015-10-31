4fb2ad7d985e50fa96b0c4f846b8b3d446fe3792
binglett <blng@sfu.ca>
2015-10-30 17:43:43 -0700

    added testing code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title =  "SLOG - Your one stop place to plan your hiking trip!"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end	
+	end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,4 +4,9 @@ class WelcomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should_get_welcome" do
+  	get :index 
+  	assert_response :success
+  	assert_select "title", "Welcome to SLOG; look for hikes here or sign up! | SLOG - Your one stop place to plan your hiking trip!"
+  end
 end
