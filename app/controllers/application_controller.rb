aac7e04c4824113140cdd4f80a5dbcae727316cf
apollo <abyoo@sfu.ca>
2015-11-05 17:45:26 -0800

    added authorization for individual pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ private
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
+
+    def authorize
+      redirect_to new_session_path, alert: "Not Authorized" if current_user.nil?
+    end
+    #https://www.youtube.com/watch?v=Hw6WtWJwRtU
 end
