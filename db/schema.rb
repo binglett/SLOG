a778d944a68d3e026177663adac7553767a9a7a5
binglett <blng@sfu.ca>
2015-11-05 17:00:28 -0800

    logged in as

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151105235936) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "username"
+    t.string   "pw"
     t.boolean  "verified"
     t.string   "email"
     t.string   "encrypted_password"
