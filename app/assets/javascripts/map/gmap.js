c4ad899e791ee49009b1df0cd0d01d9003075385
binglett <blng@sfu.ca>
2015-11-16 13:52:10 -0800

    testing geolocation for Google maps; need web server. Will delete if doesn't work on Heroku.

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -5,20 +5,48 @@ $(window).load(function() {
 
 var map;
 
-
 function initialize() {
         
   var mapOptions = {
-          center: new google.maps.LatLng(49.250000, -123.000000),
-          zoom: 10,
+          center: { lat: 49.250000, lng: -123.000000 },
+          zoom: 10,          
           mapTypeId: google.maps.MapTypeId.NORMAL,
           panControl: true,
           scaleControl: true,
           streetViewControl: true,
           overviewMapControl: true
         };
-        // initializing map
+        // initializing map with mapOptions
         map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 }
 
 function loadScript() {
