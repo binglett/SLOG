60e1260b399cb52c0cac57ba999615d20f65f463
Natireck <arseneaulttyler@gmail.com>
2015-11-23 20:42:27 -0800

    hikes and events linked together

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -43,6 +43,12 @@ function initialize() {
         // initializing map with mapOptions
         map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
         
+        var marker = new google.maps.Marker({
+		    position: { lat: glat, lng: glng },
+		    map: map
+		});
+
+        
         /*Geolocation Stuff
         var infoWindow = new google.maps.InfoWindow({map: map});
 		
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,12 @@ class Hike < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :region, presence: true
   validates :distance, presence: true
+  validates :lat, presence: true
+  validates :lng, presence: true
+  
+  has_many :events
+  #Attempt to add coord params to default root
+  #def to_param
+  #  id.to_s+"?lat="+lat.to_s+"&lng="+lng.to_s;
+  #end
 end
