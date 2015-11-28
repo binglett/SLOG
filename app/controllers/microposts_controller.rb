e705835e0f07e2316fd5f0794f8223145638f657
apollo <abyoo@sfu.ca>
2015-11-28 15:31:59 -0800

    Comment section fully functional. Need routing

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,24 @@
 class MicropostsController < ApplicationController
+  before_action :authorize, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+  end
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :events
   resources :hikes
   get 'social/social_list'
-  
-  
+
+
   resources :users
   get "sign up" => "users#new", :as => "sign_up"
   # The priority is based upon order of creation: first created -> highest priority.
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :sessions
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  resources :microposts,          only: [:create, :destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
